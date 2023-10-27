89b24db60695ab3ae06e75e017e53b23822971a4
brklntmhwk <to.be.wanryu@gmail.com>
2023-10-27 07:28:37 +0000

    set up typescript & prisma

---
+++ app/prisma/migrations/20231027064032_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
