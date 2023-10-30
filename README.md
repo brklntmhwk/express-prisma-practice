f54e39d2cab2affb73aebe74a3ad2c79a8c389c6
brklntmhwk <to.be.wanryu@gmail.com>
2023-10-30 05:08:58 +0000

    create prisma/seed.ts for seeding data

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Express Prisma Practice
-- This is
+- This repo is an example project for my practice that integrates Express, a popular Node.js framework, and Prisma, an easy-to-use ORM for Node.js & TypeScript
+- The infrastructure is set up by Dev Containers with docker and docker-compose files
+- DB is PostgreSQL
+- Template engine is Pug
+
+## Reference
+- [How can I include pug templates in compiled typescript?](https://stackoverflow.com/questions/71751998/how-can-i-include-pug-templates-in-compiled-typescript)
+- [TypeScriptのExpressアプリケーションを作る](https://qiita.com/jumperson/items/e546137f6305ea98a673)
---
+++ app/prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ log: ["query", "error", "info", "warn"] });
+
+const todoData = [
+  { title: "todo1", description: "todo1", dueDate: new Date("9999/12/31") },
+  { title: "todo2", description: "todo2", dueDate: new Date("9999/12/31") },
+  { title: "todo3", description: "todo3", dueDate: new Date("9999/12/31") },
+  { title: "todo4", description: "todo4", dueDate: new Date("9999/12/31") },
+  { title: "todo5", description: "todo5", dueDate: new Date("9999/12/31") },
+];
+
+async function main() {
+  await prisma.todo.createMany({ data: todoData, skipDuplicates: true });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
